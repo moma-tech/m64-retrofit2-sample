2b9fb1c8b1ba3413f77012af01d6db68dcad5d85
Mo <ivandjp@gmail.com>
2021-03-15 14:17:33 +0800

    add string converter

--- src/main/java/com/moma/sample/retrofit2/helper/StringHelper.java
+++ src/main/java/com/moma/sample/retrofit2/helper/StringHelper.java
@@ -119,4 +119,89 @@ public class StringHelper {
       return true;
     }
   }
+
+  /**
+   * to Snake Case
+   *
+   * @author Created by Ivan at 2021/3/15.
+   * @return String in Snake Case
+   * @param s : String
+   */
+  public static String toSnakeCase(String s) {
+    if (s == null) {
+      return null;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    boolean upperCase = false;
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+
+      boolean nextUpperCase = true;
+
+      if (i < (s.length() - 1)) {
+        nextUpperCase = Character.isUpperCase(s.charAt(i + 1));
+      }
+
+      if ((i >= 0) && Character.isUpperCase(c)) {
+        if (!upperCase || !nextUpperCase) {
+          if (i > 0) sb.append('_');
+        }
+        upperCase = true;
+      } else {
+        upperCase = false;
+      }
+
+      sb.append(Character.toLowerCase(c));
+    }
+
+    return sb.toString();
+  }
+
+  /**
+   * to Camel Case
+   *
+   * @author Created by Ivan at 2021/3/15.
+   * @return java.lang.String : Camel Case
+   * @param s : String
+   */
+  public static String toCamelCase(String s) {
+    if (s == null) {
+      return null;
+    }
+
+    s = s.toLowerCase();
+
+    StringBuilder sb = new StringBuilder(s.length());
+    boolean upperCase = false;
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+
+      if (c == '_') {
+        upperCase = true;
+      } else if (upperCase) {
+        sb.append(Character.toUpperCase(c));
+        upperCase = false;
+      } else {
+        sb.append(c);
+      }
+    }
+
+    return sb.toString();
+  }
+
+  /**
+   * To Capitalize Camel Case
+   *
+   * @author Created by Ivan at 2021/3/15.
+   * @return java.lang.String: Capitalize Camel Case
+   * @param s : String
+   */
+  public static String toCapitalizeCamelCase(String s) {
+    if (s == null) {
+      return null;
+    }
+    s = toCamelCase(s);
+    return s.substring(0, 1).toUpperCase() + s.substring(1);
+  }
 }
